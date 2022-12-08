3681be960f719e830e8c4c330da25fef96125def
Moringa School <melvin.kukwan@student.moringaschool.com>
2022-12-08 20:11:14 +0300

    finally

---
+++ index.js
@@ -0,0 +1,12 @@
+let num1 = 2000;
+let num2 = 100;
+function divide (num1, num2) {
+  return(2000/100)
+}
+const square = (num2, multiply) => {
+    return(num2*2)
+}
+
+const add = (num1, num2) =>{
+    return(num1+num2)
+}
